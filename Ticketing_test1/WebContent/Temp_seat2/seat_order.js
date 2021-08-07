804d321aa8c126aa949726a7bc64e2e9d05628e3
suhyunshim <lisa9963suhyun@gmail.com>
2021-08-08 01:10:34 +0900

    좌석 정보 적대적 높이로 수정

--- Ticketing_test1/WebContent/Temp_seat2/seat_order.js
+++ Ticketing_test1/WebContent/Temp_seat2/seat_order.js
@@ -9,8 +9,8 @@ let write_seat = document.getElemetByClassName('s_seat');
 
 
 //누른 좌석의 번호가 선택된 좌석 표시 영역에 써지도록 한다.
-const write_seat = document.getElementById('s_seat');
-function seat_click1(){
+const write_seat = document.getElementById('s_seat'); //document.querySelector('아이디 또는 클래스 이름');
+function seat_click1(){										//querySelectorAll() css 선택자로 찾은 모든 element 목록을 리턴한다.
 		write_seat.innerHTML +='<br>1번좌석 선택';
 		console.log("1번");
 }
