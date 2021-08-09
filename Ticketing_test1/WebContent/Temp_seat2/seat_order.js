f840cb149f2ec44b68b1220125206bffb33afa28
suhyunshim <lisa9963suhyun@gmail.com>
2021-08-09 12:20:21 +0900

    comment

--- Ticketing_test1/WebContent/Temp_seat2/seat_order.js
+++ Ticketing_test1/WebContent/Temp_seat2/seat_order.js
@@ -1,13 +1,3 @@
-/*let x;
-let write_seat = document.getElemetByClassName('s_seat');
-			
-			write_seat.innerHTML +='<br>1번좌석 선택';
-			break
-		case '#2':
-			write_seat.innerHTML +='<br>2번좌석 선택';*/
-
-
-
 //누른 좌석의 번호가 선택된 좌석 표시 영역에 써지도록 한다.
 const write_seat = document.getElementById('s_seat'); //document.querySelector('아이디 또는 클래스 이름');
 function seat_click1(){										//querySelectorAll() css 선택자로 찾은 모든 element 목록을 리턴한다.
@@ -19,4 +9,4 @@ function seat_click2(){
 		console.log("2번");
 }
 	
-
+//총 몇 석을 선택하고 예상 결제 금액이 얼마인지도 아래에 써주어야 한다.
