aa02339aeaf96351605dca776ce22dd44fba87aa
suhyunshim <lisa9963suhyun@gmail.com>
2021-08-16 20:53:06 +0900

    controller 구상중

--- Ticketing_test1/src/com/tbox/seatController/seatController.java
+++ Ticketing_test1/src/com/tbox/seatController/seatController.java
@@ -1,4 +1,4 @@
-package com.tbox.seatData;
+package com.tbox.seatController;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class seatData
+ * Servlet implementation class seatController
  */
-@WebServlet("/seatData")
-public class seatData extends HttpServlet {
+@WebServlet("/seatController") //.do를 쓸 것인지 안 쓸것인지는 일단 나중의 문제.
+public class seatController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public seatData() {
+    public seatController() {
         super();
         // TODO Auto-generated constructor stub
     }
