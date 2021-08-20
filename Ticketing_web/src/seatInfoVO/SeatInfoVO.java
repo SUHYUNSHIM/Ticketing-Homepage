8340bb4eb15f8d6779f52ffb471b907941157795
suhyunshim <lisa9963suhyun@gmail.com>
2021-08-20 20:06:07 +0900

    4개초과로 눌러도 기존 선택한 리스트(4개)까지는 유지되도록 수정. toggle css 부분은 좀 더 봐야함.

--- Ticketing_web/src/seatInfoVO/SeatInfoVO.java
+++ Ticketing_web/src/seatInfoVO/SeatInfoVO.java
@@ -4,7 +4,7 @@ import java.sql.Date;
 public class SeatInfoVO {
 
 	private String select_seat; //좌석번호
-	//private int num;   //선택한 좌석 수 
+	//private int num;   //선택한 좌석 수 --> count 컬럼으로 해결.
 	//private String seat_class; //좌석 등급 -> 결제가격 매기기 위해서 
 
 	//일단 클래스 제외, 좌석 정보만.
